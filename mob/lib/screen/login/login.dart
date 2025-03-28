04cb9404850d05ffc91414b5c0307cedfbfb2ddc
Juna Im <dlawnsdk04@naver.com>
2025-03-28 20:49:39 +0900

    구글 로그인 버튼

--- mob/lib/screen/login/login.dart
+++ mob/lib/screen/login/login.dart
@@ -27,6 +27,7 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: PRIMARY_COLOR,
@@ -41,65 +42,71 @@ class _LoginState extends State<Login> {
             return const Center(child: CircularProgressIndicator());
           }
           return Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: EdgeInsets.only(
+              top:  MediaQuery.of(context).size.height * 0.05,
+              left: MediaQuery.of(context).size.width * 0.05,
+              right: MediaQuery.of(context).size.width * 0.05,
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 // 아이디 입력 필드
-                TextField(
-                  controller: _userIdController,
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    labelText: AppLocalizations.of(context)!.id,
-                    errorText: _userIdErrorText.isNotEmpty ? _userIdErrorText : null,
-                  ),
-                ),
+                // TextField(
+                //   controller: _userIdController,
+                //   obscureText: false,
+                //   decoration: InputDecoration(
+                //     labelText: AppLocalizations.of(context)!.id,
+                //     errorText: _userIdErrorText.isNotEmpty ? _userIdErrorText : null,
+                //   ),
+                // ),
                 SizedBox(height: MediaQuery.of(context).size.width * 0.05),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: AppLocalizations.of(context)!.password,
-                    errorText: _userPasswordErrorText.isNotEmpty ? _userPasswordErrorText : null,
-                  ),
-                ),
+                // TextField(
+                //   controller: _passwordController,
+                //   obscureText: true,
+                //   decoration: InputDecoration(
+                //     labelText: AppLocalizations.of(context)!.password,
+                //     errorText: _userPasswordErrorText.isNotEmpty ? _userPasswordErrorText : null,
+                //   ),
+                // ),
                 SizedBox(height: MediaQuery.of(context).size.width * 0.1),
-                ElevatedButton(
-                  onPressed: () {
-                    login(_userIdController.text, _passwordController.text, context, viewModel);
-                  },
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(PRIMARY_COLOR)),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Text(
-                        AppLocalizations.of(context)!.login,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: TEXT_COLOR_W),
-                    ),
+                // ElevatedButton(
+                //   onPressed: () {
+                //     login(_userIdController.text, _passwordController.text, context, viewModel);
+                //   },
+                //   style: ButtonStyle(backgroundColor: MaterialStateProperty.all(PRIMARY_COLOR)),
+                //   child: SizedBox(
+                //     width: MediaQuery.of(context).size.width * 0.5,
+                //     child: Text(
+                //         AppLocalizations.of(context)!.login,
+                //       textAlign: TextAlign.center,
+                //       style: TextStyle(color: TEXT_COLOR_W),
+                //     ),
+                //   ),
+                // ),
+                SizedBox(height: MediaQuery.of(context).size.width * 0.02),
+                Center(
+                  child: SignInButton(
+                    Buttons.Google,
+                    text: "Google로 로그인",
+                    onPressed: googleLogin,
                   ),
                 ),
-                SizedBox(height: MediaQuery.of(context).size.width * 0.02),
-                SignInButton(
-                  Buttons.Google,
-                  text: "Google로 로그인",
-                  onPressed: googleLogin,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.width * 0.02),
+                // SizedBox(height: MediaQuery.of(context).size.width * 0.02),
                 // 회원가입 버튼
-                ElevatedButton(
-                  onPressed: () {
-                    context.go('/join');
-                  },
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(PRIMARY_COLOR)),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Text(
-                      AppLocalizations.of(context)!.join,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: TEXT_COLOR_W),
-                    ),
-                  ),
-                ),
+                // ElevatedButton(
+                //   onPressed: () {
+                //     context.go('/join');
+                //   },
+                //   style: ButtonStyle(backgroundColor: MaterialStateProperty.all(PRIMARY_COLOR)),
+                //   child: SizedBox(
+                //     width: MediaQuery.of(context).size.width * 0.5,
+                //     child: Text(
+                //       AppLocalizations.of(context)!.join,
+                //       textAlign: TextAlign.center,
+                //       style: TextStyle(color: TEXT_COLOR_W),
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           );
@@ -109,7 +116,7 @@ class _LoginState extends State<Login> {
   }
 
   login(String userId, String password, BuildContext context, UserViewModel viewModel) async {
-    // validation(userId, password);
+    validation(userId, password);
 
     if (userId.isNotEmpty && password.isNotEmpty) {
       logger("로그인중입니다.");
